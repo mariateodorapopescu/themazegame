bf85a3ef8f4dd08a1a3501fb1de232c1a06cad24
AlfredPietraru <alfred.pietraru@gmail.com>
2024-11-05 17:41:12 +0200

    moved multiple files for creating folder structure for client

--- client/client.py
+++ client/client.py
@@ -4,6 +4,10 @@ from agent import AgentRandom
 from agent_astar import AStarAgent
 from agent_dfs import DFSAgent
 import json
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from constants import Constants
 
 class Client():
